9cc2d3805299cecd2c60c73da6cf0db9ccc03287
Matheus Martins Scheffer <matheus.scheffer@dbccompany.com.br>
2024-03-06 09:49:33 -0300

    teste

--- teste.py
+++ teste.py
@@ -1,12 +1,13 @@
 import sys
 import pandas as pd
 
-arg ={'arquivo': sys.argv[1]}
-
+arg = {'arquivo': sys.argv[1]}
+df_object = {'coluna 1': sys.argv}
 
 print(arg['arquivo'])
+print(f'df_obj {df_object}')
 
-pd.DataFrame(arg).to_excel('teste.xlsx')
+pd.DataFrame(df_object).to_excel('teste.xlsx')
 
 f = open(arg['arquivo'])
 
